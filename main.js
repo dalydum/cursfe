54609a5f56c4a1f84ce8a82728bbbb6418ca8b14
Daniel D <dalydum@yahoo.com>
2021-11-23 18:09:50 +0200

    tema

---
+++ main.js
@@ -0,0 +1,25 @@
+main.js
+console.log('salut');
+console.log('tuturor');
+console.log('salut');
+
+
+
+
+let a = 4;
+let persoana = 'tudor';
+let altavariabila = 5;
+
+console.log('altavariabila');
+
+altavariabila = 5 * 3;
+
+conso.log(altavariabila);
+persoana = 'tudor' + 'dan';
+console.log(persoana);
+
+persoana = tudor + ' goia'
+console.log(persoana);
+
+
+
---
+++ tema4.js
@@ -0,0 +1,13 @@
+tema4.js
+let persoana = 'Jane Smith';
+let persoana = 'name';
+let altavariabila = $12.450;
+let altavariabila = sum;
+let variabila = 'Toyota Yaris';
+let variabila = 'car';
+let premise = 'sum is a lot of money for a car';
+let activity1 = 'plans';
+let activity2 = 'to buy';
+console.log('name + activity1 + activity2 + car');
+
+
