9f769382e72a70b9443c94354072f78dadd449a3
Daniel D <dalydum@yahoo.com>
2021-11-24 13:38:34 +0200

    all good

--- tema4.js
+++ tema4.js
@@ -1,14 +1,20 @@
-tema4.js
 let persoana = 'Jane Smith';
 let persoana = 'name';
 let altavariabila = $12.450;
 let altavariabila = sum;
 let variabila = 'Toyota Yaris';
 let variabila = 'car';
-let premise = 'sum is a lot of money for a car';
 let activity1 = 'plans';
 let activity2 = 'to buy';
+let activity3 = has;
+let premise = 'in her banck account';
+let activity4 = 'is';
+let premise1 = 'a lot of money';
+console.log('name+activity3+sum+letpremise');
+console.log('sum+let activity4 +let premise1');
 console.log('name + activity1 + activity2 + car');
+console.log('sum+letactivity4+letpremise1+car');
+
 
 
 
