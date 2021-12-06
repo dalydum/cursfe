1ce082810e8fb69bc29e5513effe3969b9b9b5af
Daniel D <dalydum@yahoo.com>
2021-12-06 18:12:06 +0200

    ko

--- domElementsPlay.js
+++ domElementsPlay.js
@@ -1,2 +1,23 @@
-let ourH1 = document.getElementsByTagName('h1');
-console.log(ourH1);
+//let allOfOurH1s = document.getElementsByTagName('h1');
+
+//let ourSingleH1 = allOfOurH1s[0];
+//console.log(ourSingleH1);
+
+//let myDiv = document.getElementById('header')
+//console.log(myDiv.children[0]);
+
+
+//ourSingleH1.innerHTML += ' (updated)';
+
+let closeButtons = document.querySelectorAll('#popup .popupHeaderClose');
+
+let ourcloseButton = closeButtons[0];
+
+ourcloseButton.addEventListener('click', function () {
+
+    let ourPopup = document.getElementById('popup');
+    ourPopup.style.display = 'none';
+});
+
+
+
--- main.js
+++ main.js
@@ -1,92 +1,20 @@
-// console.log('salut');
-// console.log('tuturor');
-// console.log('salut');
-// console.log('salut');
-// console.log('tuturor');
-// console.log('salut');
+let crewTileElement = document.getElementById('crewTile');
+let capsulesTileElement = document.getElementById('capsulesTile');
+let launchesTileElement = document.getElementById('launchesTile');
 
-
-let a = 4;
-let persoana = 'tudor';
-let altavariabila = 5;
-
-// now we will show our variable to the console
-// console.log('altavariabila');
-
-altavariabila = 5 * 3;
-
-// console.log(altavariabila);
-persoana = 'tudor' + ' dan';
-// console.log(persoana);
-
-persoana = persoana + ' goia'
-// console.log(persoana);
-
-// alert(persoana);
-
-let justAnotherOne = 5;
-justAnotherOne = justAnotherOne + 1;
-justAnotherOne = justAnotherOne + 1;
-justAnotherOne = justAnotherOne + 1;
-
-justAnotherOne += 30;
-justAnotherOne += 30;
-justAnotherOne += 30 + 10;
-
-justAnotherOne = justAnotherOne - 10;
-justAnotherOne -= 5;
-justAnotherOne -= 5;
-justAnotherOne -= 5;
-
-console.log(203 / 20);
-
-console.log(203 % 20);
-
-console.log(typeof justAnotherOne);
-
-let aStringVar = 'justa string variable';
-
-console.log(typeof aStringVar);
-
-myObject = {
-    name: 'my first object',
-    color: 'red',
-    age: 20
+let crewTileInit = function () {
+    console.log('I am the init function for the Crew tile');
 };
 
-//console.log(typeof myObject);
-//console.log(myObject.toString());
+let capsulesTileInit = function () {
+    console.log('I am the init function for the Capsules tile');
+};
 
+let launchesTileInit = function () {
+    console.log('I am the init function for the Launches tile');
+};
 
-let myFirstArray = [4, 5, 3, 'abc', 5, { a: 11 }];
-
-//console.log(typeof myFirstArray);
-//console.log(myFirstArray.toString);
-
-// console.log(justAnotherOne);
-
-//window.alert(myFirstArray);
-
-let myFirstBool;
-//console.log(typeof myFirstBool);
-
-myFirstBool = false;
-//console.log(typeof myFirstBool);
-
-console.log(typeof myFirstBool);
-
-console.log(myFirstArray.length);
-console.log(myFirstArray[2]);
-console.log(myFirstArray[3]);
-console.log(myFirstArray[5]);
-console.log(myFirstArray[5].a);
-
-console.log(myObject.name);
-console.log(myObject.age);
-
-
-
-
+crewTileElement.addEventListener('click', crewTileInit);
 
 
 
