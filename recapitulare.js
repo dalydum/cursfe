aadd9b36cb5b086880b315c77e1057afd9664444
Daniel D <dalydum@yahoo.com>
2021-12-11 14:32:33 +0200

    recap

---
+++ recapitulare.js
@@ -0,0 +1,11 @@
+let a = {
+    a: 3,
+    b: 4,
+    c: "ana"
+};
+let b = [
+    "a",
+    "b",
+    "c"
+];
+console.log(a.a);
---
+++ recapitulare1.js
@@ -0,0 +1,27 @@
+
+
+//let adunareSiImpartire = function (a, b, c) {
+//let rezultatAdunare = a + b;
+//let rezultatImpartire = rezultatAdunare / c;
+//return rezultatImpartire;
+//};
+
+//let adunareSiImpartire2 = function (a, b, c) {
+//return (a + b) / c;
+//};
+
+//console.log(adunareSiImpartire(1, 2, 3));
+//console.log(adunareSiImpartire2(1, 2, 3));
+
+//for (let i = 1; i <= 10; i = i + 1) {
+// console.log('un text' + i);
+//}
+
+//for (let i = 11; i <= 50; i = i + 3) {
+// console.log("un text " + i);
+//}
+
+for (let i = 100; i >= 50; i = i - 3) {
+    console.log("un text " + i);
+}
+
