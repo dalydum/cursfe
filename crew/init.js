594b5e20a180afb8148b91db44c3881379178ea7
dalydum <dan.ionut.dumitru@gmail.com>
2022-01-27 11:04:49 +0200

    update

--- crew/init.js
+++ crew/init.js
@@ -1,35 +1,47 @@
-let crewTileInit = function () {
+let crewTileInit = function() {
 
     let crewPopup = document.getElementById('crewPopup');
     crewPopup.style.display = 'block';
 
-    console.log('I am the init function for the CREW tile');
+    let closeButtons = document.querySelectorAll('#crewPopup .headerClose');
+    let ourCloseButton = closeButtons[0];
+    ourCloseButton.addEventListener('click', function() {
 
-    let onCrewResponse = function (response) {
+        let ourPopup = document.getElementById('crewPopup');
+        ourPopup.style.display = 'none';
+    });
+
+    let onCrewResponse = function(response) {
         let crewData = response.data;
 
-        // evrey other subsequent code that uses the spacex data will be written here
+        // every other subsequent code that uses the spacex data will be written here
         // :
 
         let nasaColElement = document.getElementById('nasaCol');
         let spacexColElement = document.getElementById('spacexCol');
 
+        nasaColElement.innerHTML = '';
+        spacexColElement.innerHTML = '';
+
         for (let i = 0; i < crewData.length; i = i + 1) {
 
-            let theCosmonaut = crewData[i];
+            let theCosmounaut = crewData[i];
 
-            if (theCosmonaut.agency === 'NASA') {
-                nasaColElement.innerHTML = nasaColElement.innerHTML + theCosmonaut.name + '<br>';
-
+            if (theCosmounaut.agency === 'NASA') {
+                nasaColElement.innerHTML = nasaColElement.innerHTML + theCosmounaut.name + '<br>';
             }
 
-            if (theCosmonaut.agency === 'SpaceX') {
-                spacexColElement.innerHTML = spacexColElement.innerHTML + theCosmonaut.name + '<br>';
-
+            if (theCosmounaut.agency === 'SpaceX') {
+                spacexColElement.innerHTML = spacexColElement.innerHTML + theCosmounaut.name + '<br>';
             }
 
         }
+
+
     };
 
     axios.get('https://api.spacexdata.com/v4/crew').then(onCrewResponse);
 };
+
+let crewTileElement = document.getElementById('crewTile');
+crewTileElement.addEventListener('click', crewTileInit);
--- main.js
+++ main.js
@@ -2,29 +2,14 @@ let crewTileElement = document.getElementById('crewTile');
 let capsulesTileElement = document.getElementById('capsulesTile');
 let launchesTileElement = document.getElementById('launchesTile');
 
-let capsulesTileInit = function () {
+let capsulesTileInit = function() {
     console.log('I am the init function for the CAPSULES tile');
 };
 
-let launchesTileInit = function () {
+let launchesTileInit = function() {
     console.log('I am the init function for the LAUNCHES tile');
 };
 
 crewTileElement.addEventListener('click', crewTileInit);
 capsulesTileElement.addEventListener('click', capsulesTileInit);
 launchesTileElement.addEventListener('click', launchesTileInit);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
